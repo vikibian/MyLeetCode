8e35fdca9c23d33dd899b306eadfe4fb6816ce33
VIKI-BIAN <710256138@qq.com>
2020-08-19 00:07:16 +0800

    第4题 看完力扣视频题解

--- src/com/neu/leetcode/problems/寻找两个正序数组的中位数_0004.java
+++ src/com/neu/leetcode/problems/寻找两个正序数组的中位数_0004.java
@@ -117,4 +117,53 @@ public class 寻找两个正序数组的中位数_0004 {
 
         return result;
     }
+
+    /**
+     * 力扣官方题解
+     */
+    public double findMedianSortedArrays2(int[] nums1, int[] nums2){
+        if (nums1.length >= nums2.length){
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+        }
+
+        int m = nums1.length;
+        int n = nums2.length;
+
+        //分割线左边的所有元素需要满足的 m + （n - m + 1） / 2
+        int totalLeft = (m + n +1)/2;
+
+        //在nums1的区间[0,m]里查找恰当的分割线
+        //使得nums1[i-1] <= nums2[j] && nums2[j - 1] <= nums1[i]
+        int left=0;
+        int right = m;
+        while (left < right){
+            int i = left + (right - left) /2;
+            int j=totalLeft - i;
+            if (nums1[i-1] > nums2[j]){
+                //下一轮搜索区间[left, i-1]
+                right = i;
+            }else {
+                //下一轮搜索区间[i, right]
+                // [left,right] 当区间只有2个元素时 一旦进入这个分支，左边届成为i的时候 这个区间就不会再缩小了 循坏就如死循环
+                left = i;
+            }
+        }
+
+        int i = left;
+        int j = totalLeft - i;
+
+        int nums1LeftMax = i==0?Integer.MIN_VALUE:nums1[i-1];
+        int nums1RightMin = i==m?Integer.MAX_VALUE:nums1[i];
+        int nums2LeftMax = j==0?Integer.MIN_VALUE:nums2[j-1];
+        int nums2RightMin = j==n?Integer.MAX_VALUE:nums2[j];
+
+        if (((m + n)%2)==1){
+            return  Math.max(nums1LeftMax,nums2LeftMax);
+        }else {
+            return (double) ((Math.max(nums1LeftMax,nums2LeftMax))+(Math.max(nums1RightMin,nums2RightMin))) / 2;
+        }
+
+    }
 }
