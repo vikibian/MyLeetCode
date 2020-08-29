3d6bf0859113a29d30ac121100a9822fa909906b
VIKI-BIAN <710256138@qq.com>
2020-08-29 23:21:21 +0800

    第7题 看完题解

--- src/com/neu/leetcode/problems/整数翻转_0007.java
+++ src/com/neu/leetcode/problems/整数翻转_0007.java
@@ -18,7 +18,8 @@ public class 整数翻转_0007 {
         int t2 = -123;
         int t3 = 120;
         int t4 = 1534236469;
-        System.out.println("result:"+reverse(t4));
+        System.out.println("result:"+reverse1(t2));
+        System.out.println("result:"+Integer.MAX_VALUE);
 
     }
 
@@ -60,5 +61,17 @@ public class 整数翻转_0007 {
         }
 
         return result;
+    }
+
+    public static int reverse1(int x) {
+        int rev = 0;
+        while (x != 0){
+            int pop = x % 10;
+            x = x / 10;
+            if (rev >= Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE/10 && pop > 7)) return 0;
+            if (rev <= Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE/10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
     }
 }
