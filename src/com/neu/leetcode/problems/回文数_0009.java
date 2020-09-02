40150033194307af9456f6fdf2fe0520b46a41e5
vikibian <710256138@qq.com>
2020-09-02 21:11:48 +0800

    第9题 题解

--- src/com/neu/leetcode/problems/回文数_0009.java
+++ src/com/neu/leetcode/problems/回文数_0009.java
@@ -25,4 +25,19 @@ public class 回文数_0009 {
 
         return flag;
     }
+
+    public static boolean isPalindrome2(int x) {
+       if (x <0 || (x%10 == 0&& x!=0)){
+           return false;
+       }
+
+       int revertedNumber = 0;
+       while (x>revertedNumber){
+           revertedNumber = revertedNumber * 10 + x % 10;
+           x = x/10;
+       }
+
+        return revertedNumber==x || x == revertedNumber / 10;
+    }
+
 }
