9ccf6f7cce78fdfea37320360420f617c8ebfc1f
vikibian <710256138@qq.com>
2020-09-10 23:46:31 +0800

    第10题 不会做

---
+++ src/com/neu/leetcode/problems/正则表达式匹配_0010.java
@@ -0,0 +1,199 @@
+package com.neu.leetcode.problems;
+
+public class 正则表达式匹配_0010 {
+    public static void main(String[] args) {
+        //false
+        String s1 = "aa";
+        String p1 = "a";
+
+        //true
+        String s2 = "aa";
+        String p2 = "a*";
+
+        //true
+        String s3 = "ab";
+        String p3 = ".*";
+
+        //true
+        String s4 = "aab";
+        String p4 = "c*a*b";
+
+        //false
+        String s5 = "mississippi";
+        String p5 = "mis*is*p*.";
+
+        //true
+        String s6 = "mississippi" ;
+        String p6 = "mis*is*ip*.";
+
+        //false
+        String s7 = "ab";
+        String p7 = ".*c";
+
+        //true
+        String s8 = "aaa";
+        String p8 = "ab*a*c*a";
+
+        System.out.println("false:"+isMatch(s1,p1));
+        System.out.println("true:"+isMatch(s2,p2));
+        System.out.println("true:"+isMatch(s3,p3));
+        System.out.println("true:"+isMatch(s4,p4));
+        System.out.println("false:"+isMatch(s5,p5));
+        System.out.println("true:"+isMatch(s6,p6));
+        System.out.println("false:"+isMatch(s7,p7));
+        System.out.println("true:"+isMatch(s8,p8));
+
+//        String[] split = p2.split("\\*");
+//
+//        System.out.println(split.length);
+//        System.out.println(split[0]);
+
+    }
+
+    public static boolean isMatch(String s, String p) {
+
+        //不包含* 则进行长度判断 长度不等则一定是false
+        if (!p.contains("*")){
+            return p.length() == s.length() ? true : false;
+        }
+
+
+
+        String[] split = p.split("\\*");
+
+        int head = 0;
+        int tail = 0;
+//        int index  = 0;
+        String target = "";
+//        String targetPre = "";
+//        String targetSuffix = "";
+
+        for (int i=0;i<split.length;i++){
+            target=split[i];
+//            targetSuffix = split[i].substring(split[i].length()-1,split[i].length());
+//            targetPre = split[i].substring(0,split[i].length()-1);
+
+            int l = 0;
+            while (l<target.length()){
+                if (l != target.length() -1){
+                    if (target.charAt(l) == s.charAt(head)){
+                        l++;
+                        head++;
+                    }else {
+                        return false;
+                    }
+                }else {
+
+
+                    if (head<s.length() && (target.charAt(l) == s.charAt(head) || target.charAt(l) == '.' )){
+                        //判断是否是最后一个数组
+                        if (i == split.length-1 && p.charAt(p.length()-1) != '*'){
+                            //最后不是*则l需要加一 否则不加
+                            head++;
+                            l++;
+                        }
+                        else {
+                            head++;
+                        }
+
+                    }
+                    else if (head == s.length() && p.charAt(p.length()-1) != '*' && p.charAt(p.length()-1) != '.' && p.charAt(p.length()-1) != s.charAt(head-1) ){
+                        return false;
+                    }
+                    else {
+                        //break while
+                        l++;
+                    }
+
+
+                }
+            }
+//            tail = tail + split[i].length() ;
+
+        }
+//        tail = tail + split.length-1;
+
+//        while (tail < p.length()){
+//
+//            if (chars[tail] == '*'){
+//                target = p.substring(head,tail+1);
+//
+//                if (target.equals(".")){
+//                    //target可以代表任意字符
+//                    target = p.substring(head,tail+1);
+//
+//                    head = tail + 1;
+//                }else {
+//
+//                }
+//
+//                int index = 0;
+//                int count = 0;
+//
+//                head = tail+1;
+//            }
+//            tail++;
+//        }
+
+//        for (int i=0;i<split.length;i++){
+//            target=split[i];
+//            targetSuffix = split[i].substring(split[i].length()-1,split[i].length());
+//            targetPre = split[i].substring(0,split[i].length()-1);
+//            while (head<s.length()){
+//                String temp = "";
+//                if (head + target.length()-1 >=s.length()){
+//                    tail = s.length();
+//                    temp = s.substring(head,tail);
+//                } else {
+//                   temp = s.substring(head,head+target.length()-1);
+//                }
+//
+//                if (i == 0){
+//                    //当第一次循环判断时 如果有相等的string 则记录匹配的位置
+//                   if (temp.equals(targetPre) && index == 0){
+//                       index = target.length();
+//                   } else {
+//                       break;
+//                   }
+//                } else if (i == split.length-1){
+//                    if (target!= "."){
+//                        if (!temp.equals(target)){
+//                            return false;
+//                        }
+//                    }
+//                } else {
+//                    if (index!=0){
+//                        //index 不为0 则表示 前面的spilt中的string匹配过
+//                        if (!temp.equals(target)){
+//                            return false;
+//                        }
+//                    }
+//                }
+//                head = head + target.length();
+//            }
+//        }
+
+//        for (int i=0;i<split.length;i++){
+//            target=split[i];
+//            targetSuffix = split[i].substring(split[i].length()-1,split[i].length());
+//            targetPre = split[i].substring(0,split[i].length()-1);
+//            while (head<s.length()){
+//                String temp = "";
+//            }
+//        }
+
+//        int index1 = 0;
+//        int index2 = 0;
+//        int index3 = 0;
+//
+//        while (index2 < p.length()){
+//
+//        }
+
+        if (head == s.length() ){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
