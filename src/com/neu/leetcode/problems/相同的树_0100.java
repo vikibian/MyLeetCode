13616a8c905c7dbfa848617838f55277bf07a525
vikibian <710256138@qq.com>
2021-01-11 16:36:42 +0800

    100题-相同的树  true  2种题解

--- src/com/neu/leetcode/problems/相同的树_0100.java
+++ src/com/neu/leetcode/problems/相同的树_0100.java
@@ -5,33 +5,33 @@ import java.util.Queue;
 
 public class 相同的树_0100 {
 
-//    public boolean isSameTree(TreeNode p, TreeNode q) {
-//
-//        return preOrder(p,q);
-//    }
-//
-//    public boolean preOrder(TreeNode p,TreeNode q){
-//
-//        if (p == null && q ==null){
-//            return true;
-//        } else if (p != null && q != null){
-//            if ((p.val != q.val)){
-//                return false;
-//            }
-//            if (!preOrder(p.left,q.left)){
-//                return false;
-//            }
-//
-//            if (!preOrder(p.right,q.right)){
-//                return false;
-//            }
-//
-//        } else {
-//            return false;
-//        }
-//
-//        return true;
-//    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        return preOrder(p,q);
+    }
+
+    public boolean preOrder(TreeNode p,TreeNode q){
+
+        if (p == null && q ==null){
+            return true;
+        } else if (p != null && q != null){
+            if ((p.val != q.val)){
+                return false;
+            }
+            if (!preOrder(p.left,q.left)){
+                return false;
+            }
+
+            if (!preOrder(p.right,q.right)){
+                return false;
+            }
+
+        } else {
+            return false;
+        }
+
+        return true;
+    }
     //官方题解1 ： 深度优先搜索
 //    public boolean isSameTree(TreeNode p, TreeNode q) {
 //        if (p == null && q == null) {
@@ -46,53 +46,53 @@ public class 相同的树_0100 {
 //    }
 
     //官方题解2
-    public boolean isSameTree(TreeNode p,TreeNode q){
-        if (p == null && q == null){
-            return true;
-        } else if (p == null || q == null){
-            return false;
-        }
-
-        Queue<TreeNode> queue1 = new LinkedList<>();
-        Queue<TreeNode> queue2 = new LinkedList<>();
-        queue1.offer(p);
-        queue2.offer(q);
-
-        while (!queue1.isEmpty() && !queue2.isEmpty()){
-            TreeNode node1 = queue1.poll();
-            TreeNode node2 = queue2.poll();
-            if (node1.val != node2.val){
-                return false;
-            }
-
-            TreeNode left1 = node1.left,right1 = node1.right,left2 = node2.left,right2 = node2.right;
-            //异或操作台机智了
-            if (left1 == null ^ left2 == null){
-                return false;
-            }
-
-            if (right1 == null ^ right2 == null){
-                return false;
-            }
-
-            if (left1 != null){
-                queue1.offer(left1);
-            }
-
-            if (right1 != null){
-                queue1.offer(right1);
-            }
-
-            if (left2 != null){
-                queue2.add(left2);
-            }
-            if (right2 != null){
-                queue2.add(right2);
-            }
-        }
-
-        return queue1.isEmpty() && queue2.isEmpty();
-    }
+//    public boolean isSameTree(TreeNode p,TreeNode q){
+//        if (p == null && q == null){
+//            return true;
+//        } else if (p == null || q == null){
+//            return false;
+//        }
+//
+//        Queue<TreeNode> queue1 = new LinkedList<>();
+//        Queue<TreeNode> queue2 = new LinkedList<>();
+//        queue1.offer(p);
+//        queue2.offer(q);
+//
+//        while (!queue1.isEmpty() && !queue2.isEmpty()){
+//            TreeNode node1 = queue1.poll();
+//            TreeNode node2 = queue2.poll();
+//            if (node1.val != node2.val){
+//                return false;
+//            }
+//
+//            TreeNode left1 = node1.left,right1 = node1.right,left2 = node2.left,right2 = node2.right;
+//            //异或操作台机智了
+//            if (left1 == null ^ left2 == null){
+//                return false;
+//            }
+//
+//            if (right1 == null ^ right2 == null){
+//                return false;
+//            }
+//
+//            if (left1 != null){
+//                queue1.offer(left1);
+//            }
+//
+//            if (right1 != null){
+//                queue1.offer(right1);
+//            }
+//
+//            if (left2 != null){
+//                queue2.add(left2);
+//            }
+//            if (right2 != null){
+//                queue2.add(right2);
+//            }
+//        }
+//
+//        return queue1.isEmpty() && queue2.isEmpty();
+//    }
 
 
  class TreeNode {
