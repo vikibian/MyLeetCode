2cec495ca19d5bff2a410ea7013e51ded8aeebd1
vikibian <710256138@qq.com>
2021-01-18 21:28:43 +0800

    106题-从中序和后序遍历序列构造二叉树  true 看完题解

--- src/com/neu/leetcode/problems/tree/从中序和后序遍历序列构造二叉树_0106.java
+++ src/com/neu/leetcode/problems/tree/从中序和后序遍历序列构造二叉树_0106.java
@@ -1,6 +1,8 @@
 package com.neu.leetcode.problems.tree;
 
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 public class 从中序和后序遍历序列构造二叉树_0106 {
@@ -81,6 +83,36 @@ public class 从中序和后序遍历序列构造二叉树_0106 {
 
 
         return null;
+    }
+
+    //官方题解 迭代
+    public TreeNode buildTree3(int[] inorder,int[] postorder){
+
+        if (inorder == null || inorder.length == 0){
+            return  null;
+        }
+
+        TreeNode root = new TreeNode(postorder[postorder.length - 1]);
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        int inorderIndex = inorder.length -1;
+        for (int i = postorder.length - 2;i>=0;i--){
+            int postorderVal = postorder[i];
+            TreeNode node = stack.peek();
+            if (node.val != inorder[inorderIndex]){
+                node.right = new TreeNode(postorderVal);
+                stack.push(node.right);
+            } else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]){
+                    node = stack.pop();
+                    inorderIndex--;
+                }
+                node.left = new TreeNode(postorderVal);
+                stack.push(node.left);
+            }
+        }
+
+        return root;
     }
 
     static class TreeNode {
