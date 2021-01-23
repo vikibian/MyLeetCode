c259e0c4f52f1c877ffb8b35bc7568a66c67410d
vikibian <710256138@qq.com>
2021-01-23 23:19:29 +0800

    94题-二叉树的中序遍历  true morris题解遍历没看

--- src/com/neu/leetcode/problems/二叉树的中序遍历_0094.java
+++ src/com/neu/leetcode/problems/二叉树的中序遍历_0094.java
@@ -1,6 +1,8 @@
 package com.neu.leetcode.problems;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 public class 二叉树的中序遍历_0094 {
@@ -17,6 +19,26 @@ public class 二叉树的中序遍历_0094 {
         list.addAll(inorderTraversal(root.left));
         list.add(root.val);
         list.addAll(inorderTraversal(root.right));
+        return list;
+    }
+
+    //官方题解 迭代
+    public List<Integer> inorderTraversal1(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null){
+            return list;
+        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode node = root;
+        while (node != null || !stack.isEmpty()){
+            while (node!= null){
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            list.add(node.val);
+            node = node.right;
+        }
         return list;
     }
 }
