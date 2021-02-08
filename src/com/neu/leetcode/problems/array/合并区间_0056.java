e64ec186a17471f170ff8b3e69bba11f0f880232
vikibian <710256138@qq.com>
2021-02-08 10:38:59 +0800

    56-合并区间 false  解法看完  中等

--- src/com/neu/leetcode/problems/array/合并区间_0056.java
+++ src/com/neu/leetcode/problems/array/合并区间_0056.java
@@ -135,7 +135,7 @@ public class 合并区间_0056 {
         List<int[]> merged = new ArrayList<>();
         for (int i=0;i<intervals.length;i++){
             int L = intervals[i][0],R = intervals[i][1];
-            if (merged.size() == 0 || merged.get(merged.size()-1)[0] < L){
+            if (merged.size() == 0 || merged.get(merged.size()-1)[1] < L){
                 merged.add(new int[]{L,R});
             } else {
                 merged.get(merged.size()-1)[1] = Math.max(merged.get(merged.size()-1)[1],R);
