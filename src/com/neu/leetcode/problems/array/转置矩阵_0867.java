ea7a25f748c3e839fa1a4e8f24fc49ca63ceae81
vikibian <710256138@qq.com>
2021-02-25 09:22:42 +0800

    867-转置矩阵  true   解法看完  简单

---
+++ src/com/neu/leetcode/problems/array/转置矩阵_0867.java
@@ -0,0 +1,15 @@
+package com.neu.leetcode.problems.array;
+
+public class 转置矩阵_0867 {
+    public int[][] transpose(int[][] matrix) {
+        int m = matrix.length;
+        int n =matrix[0].length;
+        int[][] ans = new int[n][m];
+        for (int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        return ans;
+    }
+}
