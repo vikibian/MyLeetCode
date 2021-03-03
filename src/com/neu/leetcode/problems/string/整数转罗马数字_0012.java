2019639b20e0f5276b1a8f89100cf5d86a7b7f35
vikibian <710256138@qq.com>
2021-03-03 20:45:22 +0800

    12-整数转罗马数字  true 题解看完  中等

--- src/com/neu/leetcode/problems/string/整数转罗马数字_0012.java
+++ src/com/neu/leetcode/problems/string/整数转罗马数字_0012.java
@@ -87,4 +87,28 @@ public class 整数转罗马数字_0012 {
         }
         return res;
     }
+
+    //贪心算法
+    int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    public  String intToRoman1(int num) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i=0;i<values.length && num>0;i++){
+            while (values[i]<=num){
+                num -= values[i];
+                stringBuilder.append(symbols[i]);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    //硬编码
+    public  String intToRoman2(int num) {
+        String[] thousands = {"", "M", "MM", "MMM"};
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+        return thousands[num/1000]+hundreds[num%1000/100]+tens[num%100/10] + ones[num%10];
+    }
 }
