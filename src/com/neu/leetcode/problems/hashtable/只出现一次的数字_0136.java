d41a5104f77231a081158833f9bf7c9a6213d689
vikibian <710256138@qq.com>
2021-03-13 10:32:13 +0800

    136-只出现一次的数字  true  看完题解 简单

--- src/com/neu/leetcode/problems/hashtable/只出现一次的数字_0136.java
+++ src/com/neu/leetcode/problems/hashtable/只出现一次的数字_0136.java
@@ -3,6 +3,13 @@ package com.neu.leetcode.problems.hashtable;
 import java.util.Arrays;
 
 public class 只出现一次的数字_0136 {
+
+    public static void main(String[] args) {
+//        int[] nums = {2,2,1};
+        int[] nums = {4,1,2,1,2};
+        System.out.println(singleNumber1(nums));
+    }
+
     public int singleNumber(int[] nums) {
         Arrays.sort(nums);
         int len = nums.length;
@@ -22,5 +29,19 @@ public class 只出现一次的数字_0136 {
         }
 
         return count;
+    }
+
+    //第一种方法 使用hashmap记录出现的个数 然后对map进行遍历
+
+    //第二种方法 使用list进行遍历 出现一次就加入list中  再出现一次就把list中已经有的数字删除
+
+    //第三种方法  异或
+    public static int singleNumber1(int[] nums) {
+        int single = 0;
+        for (int num : nums){
+            single ^= num;
+        }
+
+        return single;
     }
 }
